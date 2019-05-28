6f88f0ba3d9dd3fdaf549a0d670153c3b8923495
春田石雨 <flowke@163.com>
2019-05-28 09:22:25 +0800

    babel

--- cli/config/babel.config.js
+++ cli/config/babel.config.js
@@ -1,16 +1,23 @@
 const path = require('path');
 
-module.exports = function () {
+module.exports = function (cfg) {
+
+  
+
   return {
     cwd: path.resolve(__dirname, '../../'),
     presets: [
-      "@babel/preset-env"
+      ["@babel/preset-env",{
+        modules: false,
+        useBuiltIns: 'usage',
+        ...cfg.env || {}
+      }]
     ],
-    // presets: [
-    //   "@babel/preset-env",
-    //   "@babel/preset-react",
-    // ],
+
     plugins: [
+      "@babel/plugin-syntax-jsx",
+      "babel-plugin-transform-vue-jsx",
+      ["@babel/plugin-transform-runtime"],
       ["@babel/plugin-proposal-decorators",{decoratorsBeforeExport: true}],
       "@babel/plugin-proposal-class-properties",
       "@babel/plugin-proposal-optional-chaining",
